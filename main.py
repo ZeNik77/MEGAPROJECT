0830c99ea96ac77743e0de7867ec98883363127b
ZeNik77 <42634950+ZeNik77@users.noreply.github.com>
2022-11-01 16:48:37 +0200

    ненавижу жизнь

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ class MyWidget(QMainWindow, Form):
         self.flag = {}
         self.groupBox_2.hide()
         self.groupBox_3.hide()
+        self.groupBox_4.hide()
         self.flag[self.groupBox_2] = False
         self.flag[self.groupBox_3] = False
+        self.flag[self.groupBox_4] = False
         self.AboutButton.clicked.connect(self.shide_GB2)
         self.TasksButton.clicked.connect(self.shide_GB3)
+        self.CalendarButton.clicked.connect(self.shide_GB4)
         font_GB = QFont('Manrope', 24)
         font_labels = QFont('Manrope', 18)
+        font_calendar = QFont('Manrope', 14)
         self.groupBox_2.setFont(font_GB)
         self.label_name.setFont(font_labels)
         self.label_buttons.setFont(font_labels)
@@ -25,10 +29,28 @@ class MyWidget(QMainWindow, Form):
         self.btn_todo.setFont(font_labels)
         self.btn_inprocess.setFont(font_labels)
         self.btn_done.setFont(font_labels)
+        
         self.groupBox_3.setFont(font_GB)
         self.label_todo.setFont(font_labels)
         self.label_inprocess.setFont(font_labels)
         self.label_done.setFont(font_labels)
+
+        self.groupBox_4.setFont(font_GB)
+        self.label_week.setFont(font_calendar)
+        self.label_Monday.setFont(font_calendar)
+        self.label_Tuesday.setFont(font_calendar)
+        self.label_Wednesday.setFont(font_calendar)
+        self.label_Thursday.setFont(font_calendar)
+        self.label_Friday.setFont(font_calendar)
+        self.label_Saturday.setFont(font_calendar)
+        self.label_Sunday.setFont(font_calendar)
+        self.label_w1.setFont(font_calendar)
+        self.label_w2.setFont(font_calendar)
+        self.label_w3.setFont(font_calendar)
+        self.label_w4.setFont(font_calendar)
+        self.label_w5.setFont(font_calendar)
+
+
     
     def shide_GB2(self):
         if self.flag[self.groupBox_2]:
@@ -42,6 +64,12 @@ class MyWidget(QMainWindow, Form):
         else:
             self.groupBox_3.show()
         self.flag[self.groupBox_3] = not self.flag[self.groupBox_3]
+    def shide_GB4(self):
+        if self.flag[self.groupBox_4]:
+            self.groupBox_4.hide()
+        else:
+            self.groupBox_4.show()
+        self.flag[self.groupBox_4] = not self.flag[self.groupBox_4]
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
