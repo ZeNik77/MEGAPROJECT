863487a5e9be733c0b270afd44b84212236f15cc
ZeNik77 <42634950+ZeNik77@users.noreply.github.com>
2022-11-04 14:51:48 +0200

    короче добавление в расписание или как мы там это назвали

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QSpacerItem, QSizePolicy
 from PyQt5.QtGui import QFontDatabase, QFont
 from form import Ui_MainForm as Form
 
@@ -20,20 +20,23 @@ class MyWidget(QMainWindow, Form):
         self.TasksButton.clicked.connect(self.shide_GB3)
         self.CalendarButton.clicked.connect(self.shide_GB4)
         font_GB = QFont('Manrope', 24)
-        font_labels = QFont('Manrope', 18)
+        self.font_labels = QFont('Manrope', 18)
         font_calendar = QFont('Manrope', 14)
         self.groupBox_2.setFont(font_GB)
-        self.label_name.setFont(font_labels)
-        self.label_buttons.setFont(font_labels)
-        self.label_devs.setFont(font_labels)
-        self.btn_todo.setFont(font_labels)
-        self.btn_inprocess.setFont(font_labels)
-        self.btn_done.setFont(font_labels)
+        self.label_name.setFont(self.font_labels)
+        self.label_buttons.setFont(self.font_labels)
+        self.label_devs.setFont(self.font_labels)
+        self.btn_todo.setFont(self.font_labels)
+        self.btn_inprocess.setFont(self.font_labels)
+        self.btn_done.setFont(self.font_labels)
         
         self.groupBox_3.setFont(font_GB)
-        self.label_todo.setFont(font_labels)
-        self.label_inprocess.setFont(font_labels)
-        self.label_done.setFont(font_labels)
+        self.label_todo.setFont(self.font_labels)
+        self.label_inprocess.setFont(self.font_labels)
+        self.label_done.setFont(self.font_labels)
+        self.lineEdit_st1.setFont(self.font_labels)
+        self.lineEdit_st2.setFont(self.font_labels)
+        self.lineEdit_st3.setFont(self.font_labels)
 
         self.groupBox_4.setFont(font_GB)
         self.label_week.setFont(font_calendar)
@@ -50,8 +53,11 @@ class MyWidget(QMainWindow, Form):
         self.label_w4.setFont(font_calendar)
         self.label_w5.setFont(font_calendar)
 
+        self.btn_todo.clicked.connect(self.add_todo)
+        self.btn_inprocess.clicked.connect(self.add_inprocess)
+        self.btn_done.clicked.connect(self.add_done)
 
-    
+
     def shide_GB2(self):
         if self.flag[self.groupBox_2]:
             self.groupBox_2.hide()
@@ -70,6 +76,42 @@ class MyWidget(QMainWindow, Form):
         else:
             self.groupBox_4.show()
         self.flag[self.groupBox_4] = not self.flag[self.groupBox_4]
+
+    def add_todo(self, layout):
+        self.clearSpacer(self.layout_todo)
+        le = QLineEdit(self)
+        le.setFont(self.font_labels)
+        self.layout_todo.addWidget(le)
+        verticalSpacer = self.generate_spacer()
+        self.layout_todo.addItem(verticalSpacer)
+
+    def add_inprocess(self, layout):
+        self.clearSpacer(self.layout_inprocess)
+        le = QLineEdit(self)
+        le.setFont(self.font_labels)
+        self.layout_inprocess.addWidget(le)
+        verticalSpacer = self.generate_spacer()
+        self.layout_inprocess.addItem(verticalSpacer)
+
+    def add_done(self, layout):
+        self.clearSpacer(self.layout_done)
+        le = QLineEdit(self)
+        le.setFont(self.font_labels)
+        self.layout_done.addWidget(le)
+        verticalSpacer = self.generate_spacer()
+        self.layout_done.addItem(verticalSpacer)
+
+    def generate_spacer(self):
+        return QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+
+    def clearSpacer(self, layout):
+        if layout is not None:
+            for i in range(layout.count()):
+                item = layout.itemAt(i)
+                if isinstance(item, QSpacerItem):
+                    layout.removeItem(item)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
