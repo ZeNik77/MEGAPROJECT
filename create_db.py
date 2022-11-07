b7bc4890048bdbfcbe18f6b44e68825f98fd58ae
ZeNik77 <42634950+ZeNik77@users.noreply.github.com>
2022-11-07 21:06:32 +0200

    СДЕЛАНО УДАЛЕНИЕ ГОСПОДИ КАК ЖЕ ОНО БЕСИТ БЛЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕ ЗА ЧТО ВООБЩЕ 6 ЧАСОВ ПОТРАТИЛИ НА ТО ЧТО ЛЯМБДА ГРЕБАНАЯ ПРИНИМАЕТ НЕ ЗНАЧЕНИЕ А УКАЗАТЕЛЬ И СИДЕЛИ И ПЫТАЛИСЬ ВЫЖИТЬ МНЕ ЕЩЕ УРОКИ НАДО БЫЛО ДЕЛАТЬ А Я СИДЕЛ И ДУМАЛ ПОЧЕМУ ЖЕ ВСЕ 3 СОБЫТИЯ НА ОДИН ДЕНЬ ВЫВОДЯТСЯ С ОДИНАКОВЫМ АЙДИ БУДТО МНЕ ЕЩЕ ДЕЛАТЬ НЕФИГ ПОМИМО ЭТОГО НЕТ МНЕ ЕЩЕ ДЕЛАТЬ ДОМАШНЮЮ СТРАНИЦУ И СВОИ 3 КОЛОНКИ А Я СИЖУ И ДУМАЮ ПОЧЕМУ УДАЛЯЕТСЯ ТОЛЬКО ПОСЛЕДНИЙ ЭЛЕМЕНТ БД ММММММММММММММММММММММММММ РАДИ ТАКИХ МОМЕНТОВ И СТОИТ ЖИТЬ

--- create_db.py
+++ create_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-con = sqlite3.connect('main.sqlite3')
+con = sqlite3.connect('Assets/Databases/main.sqlite3')
 cur = con.cursor()
 cur.execute('CREATE TABLE EVENTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT DEFAULT "Название", DESCRIPTION TEXT DEFAULT "Описание", YEAR INT, MONTH INT, DAY INT, HOUR INT DEFAULT 0, MINUTE INT DEFAULT 0, DONE INT DEFAULT 0);')
 cur.execute(f'INSERT INTO EVENTS VALUES (0, "СДАТЬ ПРОЕКТ", "ПИПЕЦ ОСТАЛСЯ ОДИН ДЕНЬ НАМ ПИПЕЦ", 2022, 11, 8, 22, 0, 0)')
